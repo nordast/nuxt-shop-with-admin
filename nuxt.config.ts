c0ecf2d2180df3c7e3e51afd3e201a9191714211
nordast <nordast@gmail.com>
2024-11-26 10:17:43 +0200

    install packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/color-mode',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/icon',
+    "@nuxtjs/cloudinary",
+    'nuxt-auth-utils',
+    '@prisma/nuxt'
+  ],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  }
 })
