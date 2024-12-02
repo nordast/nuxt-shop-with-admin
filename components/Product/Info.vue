a260c22b3c15a959731e4a3779fb909567f00321
nordast <nordast@gmail.com>
2024-12-02 17:40:51 +0200

    homepage: Card Modal

---
+++ components/Product/Info.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import type { SafeProduct } from "~/types";
+import { formatPrice } from "~/utils";
+
+defineProps<{
+  data: SafeProduct;
+}>();
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold text-gray-900">{{ data.name }}</h1>
+
+    <div class="mt-3 flex items-end justify-between">
+      <div class="text-2xl text-gray-900">
+        <div class="flex items-center justify-between">
+          <div class="font-semibold">{{ formatPrice(data.price) }}</div>
+        </div>
+      </div>
+    </div>
+
+    <hr class="my-4" />
+
+    <div class="flex flex-col gap-y-6">
+      <div class="flex items-center gap-x-4">
+        <h3 class="font-semibold text-black">Size:</h3>
+        <div>{{ data.size.name }}</div>
+      </div>
+
+      <div class="flex items-center gap-x-4">
+        <h3 class="font-semibold text-black">Color:</h3>
+        <div
+          class="size-6 rounded-full border border-gray-600"
+          :style="`background-color: ${data.color.value}`"
+          :title="data.color.name"
+        ></div>
+      </div>
+    </div>
+
+    <div class="mt-10 flex items-center gap-x-3">
+      <Button class="rounded-full">
+        <Icon name="lucide:shopping-cart" class="size-4" />
+        Add to Cart
+      </Button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- components/Product/Modal.vue
+++ components/Product/Modal.vue
@@ -8,9 +8,43 @@ const { isOpen, onClose, selectedProduct } = usePreviewModal();
       v-if="selectedProduct"
       class="w-full grid grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8"
     >
-      {{ selectedProduct?.name }}
+      <div class="sm:col-span-4 lg:col-span-5">
+        <Carousel class="relative w-full">
+          <CarouselContent>
+            <CarouselItem
+              v-for="image in selectedProduct.images"
+              :key="image.id"
+            >
+              <div
+                class="flex aspect-square items-center justify-center border p-2 rounded"
+              >
+                <img
+                  :src="image.url"
+                  class="size-40 object-cover object-center"
+                  :alt="selectedProduct.name"
+                />
+              </div>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious class="previous-btn" />
+          <CarouselNext class="next-btn" />
+        </Carousel>
+      </div>
+
+      <div class="sm:col-span-8 lg:col-span-7">
+        <ProductInfo :data="selectedProduct"></ProductInfo>
+      </div>
     </div>
   </AppModal>
 </template>
 
-<style scoped></style>
+<style scoped>
+.previous-btn {
+  left: calc(50% - 40px);
+  top: calc(100% + 30px);
+}
+.next-btn {
+  left: 50%;
+  top: calc(100% + 30px);
+}
+</style>
